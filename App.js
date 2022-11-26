e1a4152d0afc60b026be97d46e3ab7a3cf8eba1a
Sobhen Sekhar Sahoo <sobhensekhar1998@gmail.com>
2022-11-26 14:16:29 +0530

    mimal app created

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.Textcolor}>Open up App.js to start working on your app!</Text>
+      <Button onPress={() => {alert('You tapped the button!'); }} title="Press Me"/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Textcolor:{
+    color:'#7fff00'
+  }
 });
